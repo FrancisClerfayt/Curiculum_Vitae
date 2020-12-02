690705bd5989921526c70b69be68de53e33ccbfa
FrancisClerfayt <francis.clerfayt@gmail.com>
2020-12-02 15:46:40 +0100

    complete revamp following a new design

--- js/script.js
+++ js/script.js
@@ -1,58 +1,15 @@
-function age() {
-  let birthday = new Date(1991, 5, 10);
-  let currentYear = new Date();
-  let age = currentYear.getYear() - birthday.getYear();
-  let month = currentYear.getMonth() - birthday.getMonth();
-  let day = currentYear.getDate() - birthday.getDate();
-  if(month < 0){
-    age--;
-  } else if (month == 0 && day < 0){
-    age--;
-  }
-  return age;
+let hamButton = document.querySelector('.ham');
+let navbar = document.querySelector('.navbar');
+let navlinks = document.querySelectorAll('.navlink');
+
+function toggleNavbar(){
+	navbar.classList.toggle('showNav');
+	let icon = document.querySelector('.ham > span');
+	icon.classList.toggle('fa-bars');
+	icon.classList.toggle('fa-times');	
 }
 
-let previous = "";
-
-function showTab(tabName) {
-
-  if(previous != ""){
-    previous.removeClass("active");
-  }
-  $(".active").removeClass("active");
-
-  switch (tabName) {
-    case "about":
-      $("#aPropos").addClass("active");
-      previous = $("#aPropos");
-      $("#linkAbout").addClass("active");
-      break;
-    case "XP":
-      $("#experience").addClass("active");
-      previous = $("#experience");
-      $("#linkExp").addClass("active");
-      break;
-    case "training":
-      $("#formation").addClass("active");
-      previous = $("#formation");
-      $("#linkTraining").addClass("active");
-      break;
-    case "skill":
-      $("#skills").addClass("active");
-      previous = $("#skills");
-      $("#linkSkill").addClass("active");
-      break;
-    case "hobby":
-      $("#hobby").addClass("active");
-      $("#linkHobby").addClass("active");
-      break;
-    default:
-      alert("Erreur : mauvaise valeur en entrée de showTab");
-      break;
-  }
-}
-
-$('document').ready(function(){
-  $("#age").text(age() + " ans.");
-  showTab("about");
+hamButton.addEventListener('click', toggleNavbar);
+navlinks.forEach(function(navLink){
+	navLink.addEventListener('click', toggleNavbar);
 });
